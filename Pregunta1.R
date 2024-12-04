e44d8bd4b0557b22ac61444277bbd7cea7780577
martinasanchez24 <martina.carmen.sanchez@estudiantat.upc.edu>
2024-12-04 18:48:46 +0100

    Update Pregunta1.R

--- Pregunta1.R
+++ Pregunta1.R
@@ -64,3 +64,76 @@ mean(Ssq100000>32)
 hist(Ssq100000*(100-1)/desv^2, prob=TRUE)
 curve(dchisq(x, 100-1), add=TRUE, col='red')
 (100-1)*32/desv^2
+
+#---------------------------------------------------------------------------------------------------------
+
+#EJS RESISTENCIAS 
+
+media <- 95.3 
+sd <- 5.7
+
+#1 se selecciona de forma aleatoria una mostra de 4 cables
+# el valor esperat de la suma de les resistences?
+n <- 4
+n*media
+#media de la suma --> n*media 
+
+#2 se selecciona de forma aleatoria una mostra de 100 cables
+# la varianza de la suma 
+n <- 100
+n*sd^2
+#varianza de la suma --> n*var 
+
+#3 es selecciona un cable de forma aleatoria 
+#prob que la resistencia no sigui menor a 103 
+#P(x>=103)= 1-P(x<103)
+1-pnorm(102, media, sd)
+
+#4 es seleccionen 4 cables de forma aleatoria 
+#quina es la probabilitat que la mitjana sigui menor que 98 
+P(X<98)
+
+pnorm(97, media, sd/sqrt(4)) 
+#la desviación de la media 
+
+#5 es seleccionen 100 cables de forma aleatoria
+#probabilitat que la varianza de las resistencias sigui més gran que 98
+#1-P(X>=98)
+1-pnorm(98, media, sd/sqrt(100))
+
+numero <- 1.085371e-06
+numero_decimal <- format(numero, scientific = FALSE)
+print(numero_decimal)
+
+#no estoy segura de esto 
+
+
+#--------------------------------------------------------------------------
+media <- 77
+sd <- 5.8
+#1. media de la media
+media <- media
+media
+
+#2 varianza de la suma 
+n<- 121
+var <- (sd^2)*n
+var
+
+#P(X>75) = 1-(PX<=75)
+1-pnorm(75, media, sd)
+
+#3 prob de que la suma del peso 
+#de la muestra no sea menor que 322
+
+#4 prob de que la SUMA sea: P(X>=322)= 1-P(X<322)
+1-pnorm(321, media*4, sqrt(4)*sd) #hay q hacer la media de la suma 
+#y sd de la suma 
+
+#5prob de que la varianza del peso de la muestra sea menor que 30kg 
+n <- 64
+var <- 29 
+gl <- n-1 
+prob_varianza <- pchisq((n-1)*var/sd^2, df = gl)
+prob_varianza
+
